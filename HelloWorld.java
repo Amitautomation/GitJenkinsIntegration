22acc8c9d3bf6028279b94c20f2268548fd62f13
AmitAutomation <sapjumbo@gmail.com>
2017-06-11 16:06:07 +0530

    2 files is added

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package JavaProject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=5;i++){
+			System.out.println("hello world.." +i);
+		}
+	}
+
+}
